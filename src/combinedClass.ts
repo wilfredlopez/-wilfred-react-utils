5daa13c36b9ff298ed9ccdae0ec82c94c110d52d
Wilfred Lopez <cubamc@gmail.com>
2020-05-15 19:07:00 -0400

    added function to combine classes

--- src/combinedClass.ts
+++ src/combinedClass.ts
@@ -30,3 +30,39 @@ const combined = Builder.build()
 
 combined.close()
 combined.navigate()
+
+export type Newable<T> = { new (...args: any[]): T }
+
+/**
+ * Combines two classes. returns a proxy between both.
+ * @param classOne class to combine with the 2nd one
+ * @param classTwo class to combine with the first one
+ * @example
+ * const Combined = combineClasses(ClassOne, ClassTwo)
+
+const classOneAndTwow = new Combined()
+
+classOneAndTwow.close() //property of class 1
+classOneAndTwow.navigate() // property of class 2
+ */
+export function combineClasses<T, C>(
+  classOne: Newable<T>,
+  classTwo: Newable<C>,
+) {
+  const testing = new classOne()
+  const builder = new classTwo()
+
+  const proxy = new Proxy<Newable<T> & Newable<C>>(builder as any, {
+    get: (target, property: keyof Newable<T> | keyof Newable<C>) => {
+      return target[property] || testing[property as keyof Newable<C>]
+    },
+  })
+  return proxy
+}
+
+const Combined = combineClasses(TestingW, Builder)
+
+const testAndBuild = new Combined()
+
+testAndBuild.close()
+testAndBuild.navigate()
